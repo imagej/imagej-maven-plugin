78a057c082769a3f25423ccddc8641f071b61cc8
Curtis Rueden <ctrueden@wisc.edu>
2015-01-16 17:03:27 -0600

    Happy New Year 2015!

--- src/main/java/net/imagej/maven/AbstractCopyJarsMojo.java
+++ src/main/java/net/imagej/maven/AbstractCopyJarsMojo.java
@@ -5,7 +5,7 @@ package net.imagej.maven;
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2012 - 2014 Board of Regents of the University of
+ * Copyright (C) 2012 - 2015 Board of Regents of the University of
  * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
  * Institute of Molecular Cell Biology and Genetics.
  * %%
