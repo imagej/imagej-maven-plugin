992f8a14e03009cfafe4a2464c0d9909e958847a
Mark Hiner <hinerm@gmail.com>
2015-07-20 10:22:54 -0500

    Add native classifier exception to version pattern
    This pattern was stripping the native classifier from jars, e.g.
    ffmpeg-windows-x86 was appearing the same as ffmpeg.jar, where the prior
    contains platform-specific binaries and the latter contains the Java
    hooks.
    
    To facilitate this native bundle + hook distribution pattern, our
    version-stripping pattern now allows several platform classifiers to
    remain in a given file name.

--- src/main/java/net/imagej/maven/AbstractCopyJarsMojo.java
+++ src/main/java/net/imagej/maven/AbstractCopyJarsMojo.java
@@ -206,11 +206,12 @@ public abstract class AbstractCopyJarsMojo extends AbstractMojo {
 
 	private final static Pattern versionPattern = Pattern.compile("(.+?)"
 		+ "(-\\d+(\\.\\d+|\\d{7})+[a-z]?\\d?(-[A-Za-z0-9.]+?|\\.GA)*?)?"
-		+ "((-(swing|swt|sources|javadoc|native))?(\\.jar(-[a-z]*)?))");
+		+ "((-(swing|swt|sources|javadoc|native|linux-x86|linux-x86_64|macosx-x86_64|windows-x86|windows-x86_64|android-arm|android-x86))?(\\.jar(-[a-z]*)?))");
 	private final static int PREFIX_INDEX = 1;
 	private final static int SUFFIX_INDEX = 5;
 
 	private static Collection<File> getEncroachingVersions(final File file) {
+		// TODO reuse FileUtils.stripFilenameVersion logic
 		final Matcher matcher = versionPattern.matcher(file.getName());
 		if (!matcher.matches()) return null;
 
