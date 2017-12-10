0b694bb1fc3f0aa3e8541de7f154c4c65add725d
Curtis Rueden <ctrueden@wisc.edu>
2017-12-10 22:28:28 +0100

    Update Travis configuration
    Actually, this component deploys releases to OSS Sonatype. Doh.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+sh travis-build.sh $encrypted_2cc575003135_key $encrypted_2cc575003135_iv
